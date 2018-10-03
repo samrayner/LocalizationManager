59a4d58ea44d40916a073d948affbfd1c360a292
Sam Rayner <sam.rayner@thefloow.com>
2018-10-03 10:51:52 +0100

    Initial Commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by Sam Rayner on 03/10/2018.
+//  Copyright © 2018 The Floow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
