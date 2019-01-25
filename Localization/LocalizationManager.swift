833d6e2434fc7e91276999f66456687a9fc1aba6
Sam Rayner <sam@samrayner.com>
2019-01-25 11:41:24 +0000

    Add SwiftLint and tests

---
+++ Localization/LocalizationManager.swift
@@ -0,0 +1,196 @@
+//
+//  LocalizationManager.swift
+//  Localization
+//
+//  Created by Sam Rayner on 03/10/2018.
+//  Copyright © 2018 The Floow. All rights reserved.
+//
+
+import UIKit
+
+class LocalizationManager {
+    typealias LanguageCode = String
+    typealias TranslationKey = String
+    typealias LanguageTranslations = [TranslationKey: String]
+    typealias Translations = [LanguageCode: LanguageTranslations]
+
+    enum Errors: Error {
+        case copy
+        case creation
+    }
+
+    private static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+    static let shared: LocalizationManager = {
+        let url = documentsDirectory.appendingPathComponent("Localization")
+        do {
+            if !FileManager.default.fileExists(atPath: url.path) {
+                try FileManager.default.createDirectory(at: url,
+                                                        withIntermediateDirectories: false,
+                                                        attributes: nil)
+            }
+            return LocalizationManager(localizationBundleDestination: url)
+        } catch {
+            return LocalizationManager()
+        }
+    }()
+
+    private let localizationBundleDestination: URL
+    private let stringsFilename: String
+    private let bundleExtension: String
+
+    init(stringsFilename: String = "Localizable.strings",
+         bundleExtension: String = "localizationBundle",
+         localizationBundleDestination: URL = documentsDirectory) {
+        self.stringsFilename = stringsFilename
+        self.bundleExtension = bundleExtension
+        self.localizationBundleDestination = localizationBundleDestination
+    }
+
+    private let lProjExtension = "lproj"
+    
+    private var plistFormat: PropertyListSerialization.PropertyListFormat = .binary
+    private let plistDecoder = PropertyListDecoder()
+    private lazy var plistEncoder: PropertyListEncoder = {
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = plistFormat
+        return encoder
+    }()
+
+    private lazy var currentBundle: Bundle = {
+        if let existingBundle = localizationBundle() { return existingBundle }
+        do {
+            return try localizationBundle(named: self.newBundleName(), from: .main)
+        } catch {
+            print("Failed to copy localizations from main Bundle to a new Bundle in the documents directory: \(error)")
+            return .main
+        }
+    }()
+
+    private func contentsOfDirectory(at url: URL) -> [URL] {
+        do {
+            return try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: [], options: [])
+        } catch {
+            return []
+        }
+    }
+
+    private func localizationBundleURLs() -> [URL] {
+        return contentsOfDirectory(at: localizationBundleDestination)
+            .filter { $0.pathExtension == bundleExtension }
+            .sorted { $0.lastPathComponent > $1.lastPathComponent }
+    }
+
+    private func lProjURLs(for bundle: Bundle) -> [URL] {
+        return contentsOfDirectory(at: bundle.bundleURL)
+            .filter { $0.pathExtension == lProjExtension }
+    }
+
+    private func localizationBundle() -> Bundle? {
+        var urls = localizationBundleURLs()
+
+        defer {
+            //clean up old bundles (shouldn't be any)
+            urls.forEach { try? FileManager.default.removeItem(at: $0) }
+        }
+
+        guard let url = urls.first,
+              let bundle = Bundle(url: url),
+              !lProjURLs(for: bundle).isEmpty else { return nil }
+
+        _ = urls.removeFirst() //don't clean up active bundle
+
+        return bundle
+    }
+
+    private func updateTranslations(_ old: Translations, with updates: Translations) -> Translations {
+        var new = old
+        for (key, value) in updates {
+            new[key] = old[key, default: [:]].merging(value) { $1 }
+        }
+        return new
+    }
+
+    private func newBundleName() -> String {
+        return "\(Date().timeIntervalSince1970).\(bundleExtension)"
+    }
+
+    private func createDirectoryForBundle(named bundleName: String) throws -> URL {
+        let docsBundleURL = localizationBundleDestination.appendingPathComponent(bundleName)
+
+        if FileManager.default.fileExists(atPath: docsBundleURL.path) {
+            try FileManager.default.removeItem(at: docsBundleURL)
+        }
+
+        try FileManager.default.createDirectory(at: docsBundleURL, withIntermediateDirectories: false, attributes: nil)
+
+        return docsBundleURL
+    }
+
+    private func localizationBundle(named bundleName: String, from sourceBundle: Bundle) throws -> Bundle {
+        let docsBundleURL = try createDirectoryForBundle(named: bundleName)
+
+        for sourceLprojURL in lProjURLs(for: sourceBundle) {
+            let sourceStringsURL = sourceLprojURL.appendingPathComponent(stringsFilename)
+            let docsLprojURL = docsBundleURL.appendingPathComponent(sourceLprojURL.lastPathComponent)
+
+            if FileManager.default.fileExists(atPath: sourceStringsURL.path) {
+                try? FileManager.default.removeItem(at: docsLprojURL) //in case already exists
+                try FileManager.default.copyItem(at: sourceLprojURL, to: docsLprojURL)
+            }
+        }
+
+        guard let newBundle = Bundle(url: docsBundleURL) else { throw LocalizationManager.Errors.copy }
+
+        return newBundle
+    }
+
+    private func translationsFromBundle(_ bundle: Bundle) throws -> Translations {
+        var translations: Translations = [:]
+
+        for sourceLprojURL in lProjURLs(for: bundle) {
+            let languageCode = sourceLprojURL.deletingPathExtension().lastPathComponent
+            let sourceStringsURL = sourceLprojURL.appendingPathComponent(stringsFilename)
+
+            if FileManager.default.isReadableFile(atPath: sourceStringsURL.path) {
+                let data = try Data(contentsOf: sourceStringsURL)
+                translations[languageCode] = try plistDecoder.decode(LanguageTranslations.self, from: data, format: &plistFormat)
+            }
+        }
+
+        return translations
+    }
+
+    private func localizationBundle(named bundleName: String, from translations: Translations) throws -> Bundle {
+        let docsBundleURL = try createDirectoryForBundle(named: bundleName)
+
+        for (languageCode, languageTranslations) in translations {
+            let lprojURL = docsBundleURL.appendingPathComponent("\(languageCode).\(lProjExtension)")
+
+            try FileManager.default.createDirectory(at: lprojURL, withIntermediateDirectories: false, attributes: nil)
+
+            let data = try plistEncoder.encode(languageTranslations)
+            try data.write(to: lprojURL.appendingPathComponent(stringsFilename), options: .atomic)
+        }
+
+        guard let newBundle = Bundle(url: docsBundleURL) else { throw LocalizationManager.Errors.creation }
+
+        return newBundle
+    }
+
+    func bundle(for languageCode: String? = nil) -> Bundle {
+        guard let languageCode = languageCode else { return currentBundle }
+        let lProjURL = lProjURLs(for: currentBundle).first { $0.deletingPathExtension().lastPathComponent == languageCode }
+        guard let url = lProjURL else { return currentBundle }
+        return Bundle(url: url) ?? currentBundle
+    }
+
+    func updateBundle(with translations: Translations) throws {
+        let existingTranslations = try translationsFromBundle(currentBundle)
+        let updatedTranslations = updateTranslations(existingTranslations, with: translations)
+        let newBundle = try localizationBundle(named: newBundleName(), from: updatedTranslations)
+        let oldBundle = currentBundle
+        currentBundle = newBundle
+        try? FileManager.default.removeItem(at: oldBundle.bundleURL) //clean up old bundle
+    }
+}
---
+++ LocalizationTests/LocalizationManagerTests.swift
@@ -0,0 +1,136 @@
+//
+//  LocalizationManagerTests.swift
+//  LocalizationTests
+//
+//  Created by Sam Rayner on 03/10/2018.
+//  Copyright © 2018 The Floow. All rights reserved.
+//
+
+import XCTest
+@testable import Localization
+
+class LocalizationManagerTests: XCTestCase {
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    static let bundlesDirectory = documentsDirectory.appendingPathComponent("testBundles")
+
+    let manager = LocalizationManager(
+        stringsFilename: "Localizable.strings",
+        bundleExtension: "testBundle",
+        localizationBundleDestination: bundlesDirectory
+    )
+
+    override class func tearDown() {
+        super.tearDown()
+        let documents = (try? FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: [], options: [])) ?? []
+        documents.forEach { try? FileManager.default.removeItem(at: $0) } //clean up all documents
+    }
+
+    override func setUp() {
+        super.setUp()
+        try? FileManager.default.createDirectory(at: LocalizationManagerTests.bundlesDirectory,
+                                                 withIntermediateDirectories: false,
+                                                 attributes: nil)
+    }
+
+    func test_bundleForLanguageCode() {
+        let defaultLanguageBundle = manager.bundle()
+        XCTAssertEqual(
+            defaultLanguageBundle.bundleURL.pathExtension,
+            "testBundle"
+        )
+
+        let parentDirectoryPath = defaultLanguageBundle.bundleURL.deletingLastPathComponent()
+        XCTAssertEqual(
+            parentDirectoryPath.lastPathComponent,
+            "testBundles"
+        )
+
+        let frBundle = manager.bundle(for: "fr")
+        XCTAssertTrue(
+            frBundle.bundleURL.path.hasSuffix(".testBundle/fr.lproj")
+        )
+    }
+
+    func test_updateBundleWithTranslations() throws {
+        XCTAssertEqual(
+            NSLocalizedString("hello",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(),
+                              comment: ""),
+            "HELLO Copied from main bundle!"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("hello",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(for: "fr"),
+                              comment: ""),
+            "BONJOUR Copied from main bundle!"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("goodbye",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(),
+                              comment: ""),
+            "BYE Copied from main bundle!"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("goodbye",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(for: "fr"),
+                              comment: ""),
+            "AUREVOIR Copied from main bundle!"
+        )
+
+        try manager.updateBundle(with: [
+            "en": [
+                "hello": "Hi"
+            ],
+            "fr": [
+                "hello": "Salut"
+            ]
+        ])
+
+        XCTAssertEqual(
+            NSLocalizedString("hello",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(),
+                              comment: ""),
+            "Hi"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("hello",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(for: "en"),
+                              comment: ""),
+            "Hi"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("hello",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(for: "fr"),
+                              comment: ""),
+            "Salut"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("goodbye",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(),
+                              comment: ""),
+            "BYE Copied from main bundle!"
+        )
+
+        XCTAssertEqual(
+            NSLocalizedString("goodbye",
+                              tableName: "Localizable",
+                              bundle: manager.bundle(for: "fr"),
+                              comment: ""),
+            "AUREVOIR Copied from main bundle!"
+        )
+    }
+}
